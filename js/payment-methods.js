2c128094d246b80889871653761a1a26db19ccba
Digisema <senrima.ms@gmail.com>
2025-08-17 01:26:01 +0800

    Update payment-methods.js

--- js/payment-methods.js
+++ js/payment-methods.js
@@ -13,20 +13,27 @@ const PAYMENT_METHODS = {
   "QRIS": {
     isDirect: true,
     providers: [
-      { name: "QRIS (segera hadir)", code: "QRIS", disabled: true }
+      { name: "QRIS (segera hadir)", code: "QR_QRISShopeePay_SP", disabled: true }
     ]
   },
   "Virtual Account": {
     isDirect: false,
     providers: [
-      { name: "Bank BRI (segera hadir)", code: "VA_BRI_xx", disabled: true }, // Sesuaikan kode 'xx' jika perlu
-      { name: "Bank BNI (segera hadir)", code: "VA_BNI_yy", disabled: true }, // Sesuaikan kode 'yy' jika perlu
+      { name: "Bank Mandiri", code: "VA_MANDIRI_M2", disabled: false }, // Sesuaikan kode 'xx' jika perlu
+      { name: "Bank BNI", code: "VA_BNI_I1", disabled: false },
+      { name: "Bank BRI", code: "VA_BRI_BR", disabled: false },
+      { name: "Bank BSI", code: "VA_BSI_BV", disabled: false },
+      { name: "Bank Permata", code: "VA_Permata_BT", disabled: false },
+      { name: "Bank CIMB Niaga", code: "VA_CIMB_B1", disabled: false },
+      { name: "ATM Bersama", code: "VA_ATMBersama_A1", disabled: false },
     ]
   },
   "E-Wallet": {
     isDirect: false,
     providers: [
-      { name: "GoPay (segera hadir)", code: "EWALLET_GOPAY_zz", disabled: true } // Sesuaikan kode 'zz' jika perlu
+      { name: "ShopeePay (segera hadir)", code: "EWALLET_ShopeePay_SA", disabled: true },
+      { name: "OVO (segera hadir)", code: "EWALLET_OVO_OV", disabled: true }, // Sesuaikan kode 'zz' jika perlu
     ]
   }
 };
+
