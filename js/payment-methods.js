960e9570e22839481e5b4ab3bd15fef0cbdbde83
Digisema <senrima.ms@gmail.com>
2025-08-21 22:50:15 +0800

    Update payment-methods.js

--- js/payment-methods.js
+++ js/payment-methods.js
@@ -21,21 +21,28 @@ const PAYMENT_METHODS = {
     providers: [
       { name: "Bank Mandiri", code: "VA_MANDIRI_M2", disabled: false }, // Sesuaikan kode 'xx' jika perlu
       { name: "Bank BNI", code: "VA_BNI_I1", disabled: false },
-      { name: "Bank BRI", code: "VA_BRI_BR", disabled: false },
+      { name: "Bank BRI (BRIVA)", code: "VA_BRI_BR", disabled: false },
       { name: "Bank BSI", code: "VA_BSI_BV", disabled: false },
       { name: "Bank Permata", code: "VA_Permata_BT", disabled: false },
       { name: "Bank CIMB Niaga", code: "VA_CIMB_B1", disabled: false },
+      { name: "Bank Mybank", code: "VA_Mybank_VA", disabled: false },
+      { name: "Bank Artha Graha", code: "VA_Artha_AG", disabled: false },
+      { name: "Bank Neo Commerce/BNC", code: "VA_BNC_NC", disabled: false },
+      { name: "Bank Sahabat Sampoerna", code: "VA_Sampoerna_S1", disabled: false },
+      { name: "Bank Danamon", code: "VA_Danamon_DM", disabled: false },
       { name: "ATM Bersama", code: "VA_ATMBersama_A1", disabled: false },
     ]
   },
-  "E-Wallet": {
+  "Retail": {
     isDirect: false,
     providers: [
-      { name: "ShopeePay (segera hadir)", code: "EWALLET_ShopeePay_SA", disabled: true },
-      { name: "OVO (segera hadir)", code: "EWALLET_OVO_OV", disabled: true }, // Sesuaikan kode 'zz' jika perlu
+      { name: "Alfamart", code: "Retail_Alfamart_FT", disabled: false },
+      { name: "Pos", code: "Retail_Pos_FT", disabled: false },
+      { name: "Pegadaian", code: "Retail_Pegadaian_FT", disabled: false },
     ]
   }
 };
+
 
 
 
